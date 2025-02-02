6014bf05aa13c101053f7db4f81a3ffa5f3270dd
Castor1983 <a0958086750@gmail.com>
2025-02-02 15:10:52 +0200

    commit patch order

--- src/modules/orders/dto/update-order.dto.ts
+++ src/modules/orders/dto/update-order.dto.ts
@@ -1,57 +1,70 @@
 import { PartialType} from '@nestjs/mapped-types';
 
-import {IsInt, IsOptional, IsString} from "class-validator";
+import {IsEnum, IsInt, IsOptional, IsString} from "class-validator";
 import {OrderEntity} from "../../../database/entities/order.entity";
 import {CourseEnum} from "../../../database/enums/course.enum";
 import {CourseFormatEnum} from "../../../database/enums/courseFormat.enum";
 import {CourseTypeEnum} from "../../../database/enums/courseType.enum";
 import {StatusEnum} from "../../../database/enums/status.enum";
+import {ApiProperty} from "@nestjs/swagger";
 
 export class UpdateOrderDto extends PartialType(OrderEntity) {
+    @ApiProperty()
     @IsOptional()
-    @IsString()
+    @IsEnum(CourseEnum)
     course?: CourseEnum;
 
+    @ApiProperty()
     @IsOptional()
-    @IsString()
+    @IsEnum(CourseFormatEnum)
     course_format?: CourseFormatEnum;
 
+    @ApiProperty()
     @IsOptional()
-    @IsString()
+    @IsEnum(CourseTypeEnum)
     course_type?: CourseTypeEnum;
 
+    @ApiProperty()
     @IsOptional()
-    @IsString()
+    @IsEnum(StatusEnum)
     status?: StatusEnum;
 
+    @ApiProperty()
     @IsOptional()
     @IsString()
     group?: string;
 
+    @ApiProperty()
     @IsOptional()
     @IsString()
     name?: string;
 
+    @ApiProperty()
     @IsOptional()
     @IsString()
     surname?: string;
 
+    @ApiProperty()
     @IsOptional()
     @IsString()
     phone?: string;
 
+    @ApiProperty()
     @IsOptional()
     @IsInt()
     age?: number;
 
+    @ApiProperty()
     @IsOptional()
     @IsString()
     email?: string;
 
+    @ApiProperty()
     @IsOptional()
     @IsInt()
     sum?: number;
 
+    @ApiProperty()
     @IsOptional()
     @IsInt()
     alreadyPaid?: number;
