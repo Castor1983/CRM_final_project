9004b5fdfe6ea81c1bf40536bf44f26d8ec4d555
Castor1983 <a0958086750@gmail.com>
2025-03-21 11:04:01 +0200

    final commit

--- src/modules/managers/dto/update-manager.dto.ts
+++ src/modules/managers/dto/update-manager.dto.ts
@@ -3,4 +3,3 @@ import { PartialType } from '@nestjs/mapped-types';
 import { BaseManagerDto } from './base-manager.dto';
 
 export class UpdateManagerDto extends PartialType(BaseManagerDto) {}
-//TODO
